def18af65678e277de1e6c2cda9a952a6dcd6565
victorlaurentiu <laurentiu_victor2001@yahoo.com>
2017-06-13 10:42:54 +0200

    rewritten BKT, coding-styled a little more

--- MazeGenerator_fromBMP/Source.cpp
+++ MazeGenerator_fromBMP/Source.cpp
@@ -9,7 +9,7 @@
 #include <iterator>	// iterate through file's bits
 using namespace std;
 
-typedef struct { int x, y; }point;
+struct point{ int x, y; };
 
 point d[] = { { 0,-1 },{ 1,0 },{ 0,1 },{ -1,0 } };	// used by solveBest and solveBKT
 
@@ -52,7 +52,7 @@ vector< vector<int> > createMatFromBMPFile(vector<unsigned char> imagedata) {
 	return mat;
 }
 
-void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, point st[500], int k, int *p, vector<unsigned char> imgdata) {
+void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, vector<point> sol, int k, vector<int> dir, vector<unsigned char> imgdata) {
 	int offset = imgdata[10];
 	int width = imgdata[18];
 	int height = imgdata[22];
@@ -61,9 +61,9 @@ void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, point st[500], i
 
 	int i;
 	for (i = 1; i < k; i++) {
-		mat[st[i].x][st[i].y] = 'x';
+		mat[sol[i].x][sol[i].y] = 'x';
 	}
-	mat[st[i].x][st[i].y] = 'x';
+	mat[sol[i].x][sol[i].y] = 'x';
 
 	k = offset;
 	for (int i = height; i >= 1; i--)
@@ -158,8 +158,7 @@ vector<point> readEndPoz(vector< vector<int> > mat) {
 	return exits;
 }
 
-void printMaze(vector< vector<int> > mat)
-{
+void printMaze(vector< vector<int> > mat) {
 	int height = mat.size() - 1;
 	int width = mat[0].size() - 1;
 	int i, j;
@@ -186,18 +185,17 @@ void printMaze(vector< vector<int> > mat)
 }
 
 // prints the maze with the path
-void tipar(vector< vector<int> > mat, point st[500], int k, int *p, int height, int width)
-{
+void tipar(vector< vector<int> > mat, vector<point> sol, int k, vector<int> dir) {
 	int i;
 	for (i = 1; i<k; i++)
 	{
-		mat[st[i].x][st[i].y] = p[i + 1];
+		mat[sol[i].x][sol[i].y] = dir[i + 1];
 
 		//system("cls");
 		//afisare(mat);
 	}
 
-	mat[st[i].x][st[i].y] = p[i];
+	mat[sol[i].x][sol[i].y] = dir[i];
 
 	system("cls");
 	printMaze(mat);
@@ -209,9 +207,10 @@ void bfs(vector< vector<int> >& mat, vector<point> exits) {
 	int m = mat[0].size() - 1;
 	queue<point> q;
 
+	// for each exit point I apply Dijkstra to calculate the distances
 	for (point E : exits) {
 		q.push(E);
-		mat[E.x][E.y] = 5;
+		mat[E.x][E.y] = 5;	// marked the end position
 
 		while (!q.empty()) {
 			point F = q.front();
@@ -243,35 +242,35 @@ void solveBest(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgd
 
 	bfs(mat, exits);
 
-	point* st = (point*)calloc((height*width / 2), sizeof(point));	// the solution vector
-	int* p		= (int*)calloc((height*width / 2), sizeof(int));	// the directions
-	if (!p || !st) { free(p);  free(st); }
+	vector<point> sol(height*width / 2);	// the solution vector
+	vector<int> dir(height*width / 2);	// the directions
 
 	// while I want to start from a different point
 	int Sx, Sy;
 	while (true) {
 		readStartPoz(mat, Sx, Sy);
-		st[1].x = Sx;
-		st[1].y = Sy;
+		sol[1].x = Sx;
+		sol[1].y = Sy;
 
 		int Cx = Sx;
 		int Cy = Sy;
 		int Vx, Vy;
 		int k = 1;
 		int k2;
-		while (mat[Cx][Cy] != 5) {
-			k2 = k;
+		while (mat[Cx][Cy] != 5) {	// while the Current position isn't the exit
+			k2 = k;	// for future checks
 			for (int i = 0; i < 4; i++) {
 				// for each neighbour
 				Vx = Cx + d[i].x;
 				Vy = Cy + d[i].y;
+				// going to the neighbour that has the distance smaller than the current point
 				if (mat[Vx][Vy] != -1 && mat[Vx][Vy] != 0 && mat[Vx][Vy] < mat[Cx][Cy]) {
 					k++;
 					Cx = Vx;
 					Cy = Vy;
-					st[k].x = Cx;
-					st[k].y = Cy;
-					p[k] = i + 1;
+					sol[k].x = Cx;
+					sol[k].y = Cy;
+					dir[k] = i + 1;
 					break;
 				}
 			}
@@ -282,104 +281,96 @@ void solveBest(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgd
 		}
 
 		if (k != k2) {
-			writeBMPFileFromMat(mat, fout, st, k, p, imgdata);
-			tipar(mat, st, k, p, height, width);
+			writeBMPFileFromMat(mat, fout, sol, k, dir, imgdata);
+			tipar(mat, sol, k, dir);
 		}
 
 		printf("Try another starting point? y/n\n");
 		if (_getch() == 'n')
 			break;
 	}
-}
 
-// 2 helper functions for BackTracking
-bool isSol(int x, int y, vector<point> exits) {
-	for (point E : exits) {
-		if (E.x == x && E.y == y)
-			return true;
-	}
-
-	return false;
+	/* clear memory */
+	mat.clear();
+	dir.clear();
+	sol.clear();
+	exits.clear();
 }
 
 // checks if the solution is valid
-int valid(vector< vector<int> > mat, point st[600], int k)
+bool valid(vector< vector<int> > mat, vector<point> sol, int k)
 {
 	int i;
-	if (mat[st[k].x][st[k].y] == -1)	// if it's a wall on this position
-		return 0;
+	if (mat[sol[k].x][sol[k].y] == -1)	// if it's a wall on this position
+		return false;
 
 	// check if the solution already has this position
 	for (i = 1; i < k; i++)
-		if (st[k].x == st[i].x && st[k].y == st[i].y)
-			return 0;
+		if (sol[k].x == sol[i].x && sol[k].y == sol[i].y)
+			return false;
 
-	return 1;
+	return true;
 }
 
 // Backtracking
 void solveBKT(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgdata) {
-
 	int height = mat.size() - 1;
 	int width = mat[1].size() - 1;
-	point *st;	// the solution vector
-	int  *p;	// the direction
-	st = (point*)calloc((height*width / 2), sizeof(point));
-	p = (int*)calloc((height*width / 2), sizeof(int));
-	if (!p || !st)
-	{
-		free(p);  free(st);
-	}
+
+	vector<point> sol(height*width / 2);	// the solution vector
+	vector<int> dir(height*width / 2);	// the directions
 
 	vector<point> exits = readEndPoz(mat); 
 	
 	int Sx, Sy;
 	readStartPoz(mat, Sx, Sy);
-	st[1].x = Sx;
-	st[1].y = Sy;
+	sol[1].x = Sx;
+	sol[1].y = Sy;
 
-	int as, ev, ok = 0;
+	printf("Calculating...\n");
+	bool isSol = false;
+
+	for (point E : exits) {
+		mat[E.x][E.y] = 5;	// marked the exits
+	}
 
 	int k = 2;
-	while (k > 1)
-	{
-		as = 1; ev = 0;
-		while (as != 0 && ev != 1)
-		{
-			if (p[k] < 4)
-			{
-				st[k].x = st[k - 1].x + d[p[k]].x;
-				st[k].y = st[k - 1].y + d[p[k]].y;
-				p[k]++;
-				ev = valid(mat, st, k);
-			}
-			else
-				as = 0;
+	while (k > 1) {	// while there are still solutions to find
+		while (dir[k] < 5) {	// for each direction
+			sol[k].x = sol[k - 1].x + d[dir[k]].x;
+			sol[k].y = sol[k - 1].y + d[dir[k]].y;
+			dir[k]++;
+			if (valid(mat, sol, k))	// check if it's a valid position for the solution
+				break;
 		}
-		if (as)
-			if (isSol(st[k].x, st[k].y, exits)) {
-				tipar(mat, st, k, p, height, width);
-				writeBMPFileFromMat(mat, fout, st, k, p, imgdata);
-				ok = 1;	// a solution was found
+		if (dir[k] < 5) {
+			if (mat[sol[k].x][sol[k].y] == 5) {	// if it's an exit on this position
+				tipar(mat, sol, k, dir);
+				writeBMPFileFromMat(mat, fout, sol, k, dir, imgdata);
+				isSol = true;	// a solution was found
 
 				printf("\nStop at this solution? y/n\n");
-				if(_getch() == 'y')
+				if (_getch() == 'y')
 					break;
 			}
 			else {
 				k++;
-				p[k] = 0;
+				dir[k] = 0;
 			}
-		else
-			k--;
+		}
+		else {
+			k--;	// go back, I ended in a dead-end
+		}
 	}
 
-	if (!ok)
+	if (!isSol)
 		printf("No solution exists\n");
 
 	/* clear memory */
 	mat.clear();
-	free(p);  free(st);
+	dir.clear();
+	sol.clear();
+	exits.clear();
 }
 
 int main() {
@@ -403,12 +394,12 @@ int main() {
 		solveBKT(mat, fout, buffer);
 	}
 
+	printf("\nPress any key\n");
+	_getch();	// waits any key
+
 	// free memory
 	buffer.clear();
 	fclose(fout);
 
-	printf("\nPress any key\n");
-	_getch();	// waits any key
-	
 	return 0;
 }
--- README.md
+++ README.md
@@ -6,7 +6,14 @@ same for Output file.
 
 It reads the image in binary mode, bit by bit. It takes the image's height, widht, offset, bpp from the file's header.
 And then checks every pixel how white or black is and creates acordingly a matrix with 0s and -1s where
-0 = way and -1 = wall.
+0 = way
+-1 = wall.
+1 = going left
+2 = going down
+3 = right
+4 = up
+5 = exits
+[6...] = the distance to the closest exit.
 After giving him some Start and End points, it searches for the path using either bfs or backtracking (user's choice).
 After showing the solution it creates a new BMP file using the bits from the first one, also marking the path 
 (colouring it in gray which is the value of white / 2).
