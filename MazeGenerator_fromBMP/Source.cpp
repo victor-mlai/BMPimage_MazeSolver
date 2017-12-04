e3df727c20556b8f18b3cf2e828c2fe49cba99ca
victorMunteanu <muntenau.victor0201@gmail.com>
2017-12-05 01:34:10 +0200

    added vec2 struct and others

--- MazeGenerator_fromBMP/Source.cpp
+++ MazeGenerator_fromBMP/Source.cpp
@@ -9,9 +9,21 @@
 #include <iterator>	// iterate through file's bits
 using namespace std;
 
-struct point{ int x, y; };
+struct vec2 {
+	int x, y;
+	vec2() {}
+	vec2(int x, int y) : x(x), y(y) {}
+	vec2 operator+ (const vec2& other) {
+		return vec2(x + other.x, y + other.y);
+	}
+	void operator= (const vec2& other) {
+		x = other.x;
+		y = other.y;
+	}
+};
 
-point d[] = { { 0,-1 },{ 1,0 },{ 0,1 },{ -1,0 } };	// used by solveBest and solveBKT for calculating the neighbour point
+vec2 d[] = { { 0,-1 },{ 1,0 },{ 0,1 },{ -1,0 } };	// used by solveBest and solveBKT for calculating the neighbour point
+vec2 maxRadius = {80, 240};
 
 vector< vector<int> > createMatFromBMPFile(vector<unsigned char> imagedata) {
 	unsigned char bpp, padding;
@@ -52,7 +64,14 @@ vector< vector<int> > createMatFromBMPFile(vector<unsigned char> imagedata) {
 	return mat;
 }
 
-void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, vector<point> sol, int k, vector<int> dir, vector<unsigned char> imgdata) {
+/*
+mat - the matrix containing 0's and 1's
+sol - array of positions representing the solution path
+k - sol array size
+dir - array of directions for each position
+imgdata - image data used to write another image using the old one
+*/
+void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, vector<vec2> sol, int k, vector<int> dir, vector<unsigned char> imgdata) {
 	unsigned char bpp, padding;
 
 	uint16_t offset = imgdata[11];
@@ -72,7 +91,7 @@ void writeBMPFileFromMat(vector< vector<int> > mat, FILE* fout, vector<point> so
 
 	// marking the path to colour it differently
 	int i;
-	point v;
+	vec2 v;
 	for (i = 1; i < k; i++) {
 		// mark neigbours to thicken the line
 		for (int j = 0; j < 4; j++) {
@@ -140,7 +159,7 @@ void printMaze(vector< vector<int> > mat) {
 	int height = mat.size() - 1;
 	int width = mat[0].size() - 1;
 	int i, j;
-	int zid = 254;
+	int zid = 182;
 	printf("\n");
 	for (j = 1; j <= width; j += 2)
 		printf("%-2d  ", j);
@@ -163,24 +182,25 @@ void printMaze(vector< vector<int> > mat) {
 }
 
 // prints a crop of the maze
-void printMazeCrop(vector< vector<int> > mat, point center, int radius) {
+void printMazeCrop(vector< vector<int> > mat, vec2 center, vec2 radius) {
 	int height = mat.size() - 1;
 	int width = mat[0].size() - 1;
 	int i, j;
-	int zid = 254;
+	int zid = 219;
 	printf("\n");
 
-	point start = { center.x - radius < 1 ? 1 : center.x - radius,center.y - radius < 1 ? 1 : center.y - radius };
-	point end = { center.x + radius > height ? height : center.x + radius,center.y + radius > width ? width : center.y + radius };
+	vec2 upLeftCorner = { center.x - radius.x < 1 ? 1 : center.x - radius.x,
+						  center.y - radius.y < 1 ? 1 : center.y - radius.y };
+	vec2 bottomRightCorner = { center.x + radius.x > height ? height : center.x + radius.x,
+							   center.y + radius.y > width ? width : center.y + radius.y };
 
-	for (j = start.y; j <= end.y; j += 2)
+	for (j = upLeftCorner.y; j <= bottomRightCorner.y; j += 2)
 		printf("%-2d  ", j);
 	printf("\n");
-	for (i = start.x; i <= end.x; i++)
+	for (i = upLeftCorner.x; i <= bottomRightCorner.x; i++)
 	{
-		for (j = start.y; j <= end.y; j++)
-			switch (mat[i][j])
-			{
+		for (j = upLeftCorner.y; j <= bottomRightCorner.y; j++) {
+			switch (mat[i][j]) {
 			case -1: printf("%c%c", zid, zid); break;
 			case 1: printf("<<"); break;
 			case 2: printf("vv"); break;
@@ -188,19 +208,57 @@ void printMazeCrop(vector< vector<int> > mat, point center, int radius) {
 			case 4: printf("^^"); break;
 			default: printf("  "); break;
 			}
+		}
 		printf(" %d\n", i);
 	}
 	printf("\n");
 }
 
+// prints a crop of the maze zoomed
+//
+//
+// TO DO
+//
+//
+//void printZoomedMazeCrop(vector< vector<int> > mat, vec2 center, int radius, int zoom) {
+//	int height = mat.size() - 1;
+//	int width = mat[0].size() - 1;
+//	int i, j;
+//	int zid = 254;
+//	printf("\n");
+//
+//	vec2 upLeftCorner = { center.x - radius < 1 ? 1 : center.x - radius, center.y - radius < 1 ? 1 : center.y - radius };
+//	vec2 bottomRightCorner = { center.x + radius > height ? height : center.x + radius, center.y + radius > width ? width : center.y + radius };
+//
+//	for (j = upLeftCorner.y; j <= bottomRightCorner.y; j += 2)
+//		printf("%-2d  ", j);
+//	printf("\n");
+//	for (i = upLeftCorner.x; i <= bottomRightCorner.x; i++)
+//	{
+//		for (j = upLeftCorner.y; j <= bottomRightCorner.y; j++) {
+//			switch (mat[i][j]) {
+//			case -1: printf("%c%c", zid, zid); break;
+//			case 1: printf("<<"); break;
+//			case 2: printf("vv"); break;
+//			case 3: printf(">>"); break;
+//			case 4: printf("^^"); break;
+//			default: printf("  "); break;
+//			}
+//		}
+//		printf(" %d\n", i);
+//	}
+//	printf("\n");
+//}
+
 void readWhatToCrop(vector< vector<int> > mat) {
-	int x, y, r;
+	vec2 center;
+	vec2 r;
 	while (true) {
 		printf("Show specific crop of the maze?\n y/n\n");
 		if (_getch() == 'y') {
-			printf("Write point coord and radius (Format: x y r)\n");
-			scanf("%d %d %d", &x, &y, &r);
-			printMazeCrop(mat, { x, y }, r);
+			printf("Write center and radius (Format: cx cy rx ry)\n");
+			scanf("%d %d %d %d", &center.x, &center.y, &r.x, &r.y);
+			printMazeCrop(mat, center, r);
 		}
 		else {
 			break;
@@ -208,24 +266,27 @@ void readWhatToCrop(vector< vector<int> > mat) {
 	}
 }
 
-void readStartPoz(vector< vector<int> > mat, int& Sx, int& Sy) {
+void readStartPoz(vector< vector<int> > mat, vec2& Start) {
 	int height = mat.size() - 1;
 	int width = mat[0].size() - 1;
 
 	readWhatToCrop(mat);
 
-tryagain:
-	printf("Write the coord for start position (Format: x y)\n");
-	scanf("%d %d", &Sx, &Sy);
+	while (true) {
+		printf("Write the coord for start position (Format: x y)\n");
+		scanf("%d %d", &Start.x, &Start.y);
 
-	if (Sx < 1 || Sx > height || Sy < 1 || Sy > width || mat[Sx][Sy] == -1)
-	{
-		printf("Outside or Wall => Try again\n");
-		goto tryagain;
+		if (Start.x < 1 || Start.x > height || Start.y < 1 || Start.y > width || mat[Start.x][Start.y] == -1)
+		{
+			printf("Outside Maze or Wall position => Try again\n");
+			continue;
+		}
+
+		break;
 	}
 }
 
-vector<point> readEndPoz(vector< vector<int> > mat) {
+vector<vec2> readEndPoz(vector< vector<int> > mat) {
 	int height = mat.size();
 	int width = mat[0].size();
 
@@ -233,61 +294,64 @@ vector<point> readEndPoz(vector< vector<int> > mat) {
 	printf("How many exits?\n");
 	scanf("%d", &nr_exits);
 
-	int Ex, Ey;	// End coordonates
-	vector<point> exits;
+	vec2 exit;	// End coordonates
+	vector<vec2> exits;
 	for (int i = 0; i < nr_exits; i++) {
-	tryagain2:
-		readWhatToCrop(mat);
-		
-		printf("Write the coord for end position (Format: x y)\n");
-		scanf("%d %d", &Ex, &Ey);
+		while (true) {
+			readWhatToCrop(mat);
 
-		if (Ex < 1 || Ex > height || Ey < 1 || Ey > width || mat[Ex][Ey] == -1)
-		{
-			printf("Outside or Wall => Try again\n");
-			goto tryagain2;
+			printf("Write the coordinates for exit position (Format: x y)\n");
+			scanf("%d %d", &exit.x, &exit.y);
+
+			if (exit.x < 1 || exit.x > height || exit.y < 1 || exit.y > width || mat[exit.x][exit.y] == -1)
+			{
+				printf("Outside Maze or Wall position => Try again\n");
+				continue;
+			}
+
+			break;
 		}
 
-		exits.push_back({ Ex, Ey });
+		exits.push_back(exit);
 	}
 
 	return exits;
 }
 
 // animates the printing of the maze's path
-void tipar(vector< vector<int> > mat, vector<point> sol, int k, vector<int> dir) {
+void tipar(vector< vector<int> > mat, vector<vec2> sol, int k, vector<int> dir) {
 	int i;
 	for (i = 1; i<k; i++)
 	{
 		mat[sol[i].x][sol[i].y] = dir[i + 1];
 
 		// comment here to show the maze directly solved
-		system("cls");
-		printMazeCrop(mat, sol[i], 20);
+		//system("cls");
+		//printMazeCrop(mat, sol[i], maxRadius);
 	}
 
 	mat[sol[i].x][sol[i].y] = dir[i];
 
 	system("cls");
-	printMazeCrop(mat, sol[i], 20);
+	printMazeCrop(mat, sol[i], maxRadius);
 }
 
 // for every point in the maze assigns the smallest distance to any exit
-void bfs(vector< vector<int> >& mat, vector<point> exits) {
+void bfs(vector< vector<int> >& mat, vector<vec2> exits) {
 	int n = mat.size() - 1;
 	int m = mat[0].size() - 1;
-	queue<point> q;
+	queue<vec2> q;
 
 	// for each exit point I apply Dijkstra to calculate the distances
-	for (point E : exits) {
+	for (vec2 E : exits) {
 		q.push(E);
 		mat[E.x][E.y] = 5;	// marked the end position
 
 		while (!q.empty()) {
-			point F = q.front();
+			vec2 F = q.front();
 			q.pop();
 
-			point V;
+			vec2 V;
 			for (int i = 0; i < 4; i++) {	// for each neighbour
 				V.x = F.x + d[i].x;
 				V.y = F.y + d[i].y;
@@ -301,8 +365,9 @@ void bfs(vector< vector<int> >& mat, vector<point> exits) {
 			}
 		}
 
-		//printMat(mat); // uncomment this to see the distances
-		//_getch();
+		// uncomment these to see the distances
+		// printMat(mat); 
+		// _getch();
 	}
 }
 
@@ -310,38 +375,32 @@ void solveBest(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgd
 	int height = mat.size() - 1;
 	int width = mat[1].size() - 1;
 	
-	vector<point> exits = readEndPoz(mat);
+	vector<vec2> exits = readEndPoz(mat);
 
 	bfs(mat, exits);
 
-	vector<point> sol(height*width / 2);	// the solution vector
+	vector<vec2> sol(height*width / 2);	// the solution vector
 	vector<int> dir(height*width / 2);	// the directions
 
 	// while I want to start from a different point
-	int Sx, Sy;
+	vec2 Start;	// Start Position
 	while (true) {
-		readStartPoz(mat, Sx, Sy);
-		sol[1].x = Sx;
-		sol[1].y = Sy;
-
-		int Cx = Sx;
-		int Cy = Sy;
-		int Vx, Vy;
+		readStartPoz(mat, Start);
+		sol[1] = Start;
+		vec2 Curr = Start;	// Current Position
+		vec2 Neigh;		// Neighbour's Position
 		int k = 1;
 		int k2 = 2;
-		while (mat[Cx][Cy] != 5) {	// while the Current position isn't the exit
+		while (mat[Curr.x][Curr.y] != 5) {	// while the Current position isn't the exit
 			k2 = k;	// for future checks
 			for (int i = 0; i < 4; i++) {
 				// for each neighbour
-				Vx = Cx + d[i].x;
-				Vy = Cy + d[i].y;
+				Neigh = Curr + d[i];
 				// going to the neighbour that has the distance smaller than the current point
-				if (mat[Vx][Vy] != -1 && mat[Vx][Vy] != 0 && mat[Vx][Vy] < mat[Cx][Cy]) {
+				if (mat[Neigh.x][Neigh.y] != -1 && mat[Neigh.x][Neigh.y] != 0 && mat[Neigh.x][Neigh.y] < mat[Curr.x][Curr.y]) {
 					k++;
-					Cx = Vx;
-					Cy = Vy;
-					sol[k].x = Cx;
-					sol[k].y = Cy;
+					Curr = Neigh;
+					sol[k] = Curr;
 					dir[k] = i + 1;
 					break;
 				}
@@ -362,10 +421,10 @@ void solveBest(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgd
 			break;
 
 		system("cls");
-		printMazeCrop(mat, { 50,50 }, 60);
+		printMazeCrop(mat, { 50,50 }, maxRadius);
 	}
 
-	/* clear memory */
+	/* clear memory . . . I hope */
 	mat.clear();
 	dir.clear();
 	sol.clear();
@@ -373,7 +432,7 @@ void solveBest(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgd
 }
 
 // checks if the solution is valid
-bool valid(vector< vector<int> > mat, vector<point> sol, int k)
+bool valid(vector< vector<int> > mat, vector<vec2> sol, int k)
 {
 	int i;
 	if (mat[sol[k].x][sol[k].y] == -1)	// if it's a wall on this position
@@ -388,65 +447,65 @@ bool valid(vector< vector<int> > mat, vector<point> sol, int k)
 }
 
 // Backtracking
-void solveBKT(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgdata) {
-	int height = mat.size() - 1;
-	int width = mat[1].size() - 1;
-
-	vector<point> sol(height*width / 2);	// the solution vector
-	vector<int> dir(height*width / 2);	// the directions
-
-	vector<point> exits = readEndPoz(mat); 
-	
-	int Sx, Sy;
-	readStartPoz(mat, Sx, Sy);
-	sol[1].x = Sx;
-	sol[1].y = Sy;
-
-	printf("Calculating...\n");
-	bool isSol = false;
-
-	for (point E : exits) {
-		mat[E.x][E.y] = 5;	// marked the exits
-	}
-
-	int k = 2;
-	while (k > 1) {	// while there are still solutions to find
-		while (dir[k] < 5) {	// for each direction
-			sol[k].x = sol[k - 1].x + d[dir[k]].x;
-			sol[k].y = sol[k - 1].y + d[dir[k]].y;
-			dir[k]++;
-			if (valid(mat, sol, k))	// check if it's a valid position for the solution
-				break;
-		}
-		if (dir[k] < 5) {
-			if (mat[sol[k].x][sol[k].y] == 5) {	// if it's an exit on this position
-				tipar(mat, sol, k, dir);
-				writeBMPFileFromMat(mat, fout, sol, k, dir, imgdata);
-				isSol = true;	// a solution was found
-
-				printf("\nStop at this solution? y/n\n");
-				if (_getch() == 'y')
-					break;
-			}
-			else {
-				k++;
-				dir[k] = 0;
-			}
-		}
-		else {
-			k--;	// go back, I ended in a dead-end
-		}
-	}
-
-	if (!isSol)
-		printf("No solution exists\n");
-
-	/* clear memory */
-	mat.clear();
-	dir.clear();
-	sol.clear();
-	exits.clear();
-}
+//void solveBKT(vector< vector<int> > mat, FILE* fout, vector<unsigned char> imgdata) {
+//	int height = mat.size() - 1;
+//	int width = mat[1].size() - 1;
+//
+//	vector<vec2> sol(height*width / 2);	// the solution vector
+//	vector<int> dir(height*width / 2);	// the directions
+//
+//	vector<vec2> exits = readEndPoz(mat); 
+//	
+//	int Sx, Sy;
+//	readStartPoz(mat, Sx, Sy);
+//	sol[1].x = Sx;
+//	sol[1].y = Sy;
+//
+//	printf("Calculating...\n");
+//	bool isSol = false;
+//
+//	for (vec2 E : exits) {
+//		mat[E.x][E.y] = 5;	// marked the exits
+//	}
+//
+//	int k = 2;
+//	while (k > 1) {	// while there are still solutions to find
+//		while (dir[k] < 5) {	// for each direction
+//			sol[k].x = sol[k - 1].x + d[dir[k]].x;
+//			sol[k].y = sol[k - 1].y + d[dir[k]].y;
+//			dir[k]++;
+//			if (valid(mat, sol, k))	// check if it's a valid position for the solution
+//				break;
+//		}
+//		if (dir[k] < 5) {
+//			if (mat[sol[k].x][sol[k].y] == 5) {	// if it's an exit on this position
+//				tipar(mat, sol, k, dir);
+//				writeBMPFileFromMat(mat, fout, sol, k, dir, imgdata);
+//				isSol = true;	// a solution was found
+//
+//				printf("\nStop at this solution? y/n\n");
+//				if (_getch() == 'y')
+//					break;
+//			}
+//			else {
+//				k++;
+//				dir[k] = 0;
+//			}
+//		}
+//		else {
+//			k--;	// go back, I ended in a dead-end
+//		}
+//	}
+//
+//	if (!isSol)
+//		printf("No solution exists\n");
+//
+//	/* clear memory */
+//	mat.clear();
+//	dir.clear();
+//	sol.clear();
+//	exits.clear();
+//}
 
 int main() {
 	char* filename = (char*)malloc(20);
@@ -488,15 +547,15 @@ int main() {
 		_getch();	// waits any key
 
 		vector< vector<int> > mat = createMatFromBMPFile(buffer);
-		printMazeCrop(mat, { 50,50 }, 60);
+		printMazeCrop(mat, { 50,50 }, maxRadius);
 
-		printf("y:Show the best solution (Recomended) or\nn:every single solution? (Note that Backtracking is very slow)\n y/n\n");
-		if (_getch() == 'y') {
+		//printf("y:Show the best solution (Recomended) or\nn:every single solution? (Note that Backtracking is very slow)\n y/n\n");
+		//if (_getch() == 'y') {
 			solveBest(mat, fout, buffer);
-		}
-		else {
-			solveBKT(mat, fout, buffer);
-		}
+		//}
+		//else {
+			//solveBKT(mat, fout, buffer);
+		//}
 
 		// free memory
 		fclose(fout);
@@ -504,9 +563,7 @@ int main() {
 		mat.clear();
 
 		printf("\nThe solution Image was created\nTry another image? y/n\n");
-		if (_getch() == 'y')
-			continue;
-		else
+		if (_getch() != 'y')
 			break;
 	}
 
